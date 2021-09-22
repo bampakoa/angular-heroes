88fdee81f927a90e567ade127efd9b54104af24c
Aristeidis Bampakos <bampakoa@gmail.com>
2021-09-22 11:14:58 +0300

    chore: cut the 1.3.1 release

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   settings: {
     appErrorPrefix: '[Angular Heroes Error] ',
     appTitle: 'Angular Heroes',
-    version: '1.3.0',
+    version: '1.3.1',
     charactersLimit: 20
   }
 };
