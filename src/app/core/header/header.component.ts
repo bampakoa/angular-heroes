7bf6e86d00f74c831d638539554b87c76fcab4e6
Aristeidis Bampakos <bampakoa@gmail.com>
2022-12-12 21:55:03 +0200

    refactor: set logo dimensions in the template

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -4,8 +4,7 @@ import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
