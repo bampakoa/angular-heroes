05f2c6c38a7df2f6591f51806e6e63858df5c757
Aristeidis Bampakos <bampakoa@gmail.com>
2023-08-14 21:00:08 +0300

    Convert core components to standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { MatSnackBarModule, MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material/snack-bar';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppErrorHandler } from './app-error-handler';
 import { AppComponent } from './app.component';
-import { appSettings, APP_CONFIG } from './app.config';
+import { APP_CONFIG, appSettings } from './app.config';
 import { AuthInterceptor } from './auth-interceptor.service';
 import { CharactersModule } from './characters/characters.module';
-import { CoreModule } from './core/core.module';
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,8 @@ import { CoreModule } from './core/core.module';
     BrowserAnimationsModule,
     BrowserModule,
     CharactersModule,
-    CoreModule,
+    FooterComponent,
+    HeaderComponent,
     HttpClientModule,
     MatSnackBarModule
   ],
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,11 +1,22 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { APP_CONFIG, AppConfig } from '../../app.config';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styles: ['h2 { flex: 1 1 auto; }']
+  styles: ['h2 { flex: 1 1 auto; }'],
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
+  ]
 })
 export class HeaderComponent implements OnInit {
 
