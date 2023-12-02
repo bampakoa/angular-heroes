e487ec55109061d9ba8069e9fc8c883b8965eb31
Aristeidis Bampakos <bampakoa@gmail.com>
2023-12-02 10:47:40 +0200

    Use strings for applying component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {}
