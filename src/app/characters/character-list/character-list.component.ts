905d8a79a9b855c047b8576680b95ae0117c12f7
Aristeidis Bampakos <bampakoa@gmail.com>
2024-11-26 00:58:39 +0200

    Use strings for applying component styles

--- src/app/characters/character-list/character-list.component.ts
+++ src/app/characters/character-list/character-list.component.ts
@@ -14,7 +14,7 @@ import { CharacterService } from '../characters.service';
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.css']
+  styleUrl: './character-list.component.css'
 })
 export class CharacterListComponent implements OnInit {
 
